19f71f5e90bbae09dffb22890397d121eb822a2d
JoaoTorpe <victotorpe10@gmail.com>
2023-08-20 16:13:37 -0300

    Page initial layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import "./App.css"
+import {Button}  from "./Button"
+
 export function App(){
 return <div>
-<div className="appTopBar" >
-    
+<div className="appTopBar">
+    <Button Name="Products"/>
+    <Button Name="Insights"/>
+    <Button Name="Suplliers"/>
 </div>
 
 </div>
---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import "./Button.css"
+
+export function Button(props){
+    return <div className='btn'>{props.Name}</div>
+    
+}
