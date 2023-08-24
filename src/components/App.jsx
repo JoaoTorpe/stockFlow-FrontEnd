1c53477035910384eb2c799a8810d9605e886622
JoaoTorpe <victotorpe10@gmail.com>
2023-08-24 18:42:40 -0300

    now the buttons can change the routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 import "./App.css"
 import {Button}  from "./Button"
-import {Outlet} from "react-router-dom"
+import {Link, Outlet} from "react-router-dom"
 export function App(){
 return <div>
 <div className="appTopBar">
+    <Link to="/products" >
     <Button Name="Products"/>
+</Link>
+    <Link to="/insights">
     <Button Name="Insights"/>
+</Link>
+<Link to="/supllier" >
     <Button Name="Suplliers"/>
+    </Link>
 </div>
 
 <section><Outlet/></section>
