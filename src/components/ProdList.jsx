195c8fbdf23e2273e041fdbca6860b931343e455
JoaoTorpe <victotorpe10@gmail.com>
2023-09-02 14:23:52 -0300

    ProductList style

--- src/components/ProdList.jsx
+++ src/components/ProdList.jsx
@@ -19,7 +19,6 @@ function ProdList({ isListOpen, setListOpen, listData }) {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     backgroundColor: 'white',
-    padding: '20px',
     boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
     minHeight:'200px',
     width:'200px',
@@ -42,7 +41,7 @@ function ProdList({ isListOpen, setListOpen, listData }) {
   };
 
   function generateLi(item) {
-    return <li style={{ listStyle: 'none'}} key={item.id}>{item.name}</li>;
+    return <li style={{ listStyle:'none'}}  key={item.id}>{item.name}</li>;
   }
 
   return (
@@ -55,7 +54,8 @@ function ProdList({ isListOpen, setListOpen, listData }) {
           <button onClick={() => setListOpen(false)} style={buttonStyle}>
           x
           </button>
-          <ul style={{display:'flex', flexDirection:'column',gap:'5px',alignItems:'center'}}  >{ listData ?   listData.map((item) => generateLi(item)) : 'none' }</ul>
+          <ul style={{position:'relative',right:'15px',gap:'5px' ,display:'flex',flexDirection:'column',alignItems:'center' }}  >
+          { listData ?   listData.map((item) => generateLi(item)) : 'none' }</ul>
         </div>
       </div>
     </div>
