d0ff7c1df8691ffac90ad2bc93a90e6b829ebf1b
JoaoTorpe <victotorpe10@gmail.com>
2023-09-06 14:39:11 -0300

    Insights page requests and hooks

--- src/routes/Insights.jsx
+++ src/routes/Insights.jsx
@@ -21,16 +21,49 @@ import axios from 'axios';
       GetTotalValue();
     },[totalValue] )
 
+    const [totalProducts,setTotalProducts] = useState(0);
+    async function  GetTotalProductsQuantity(){
+      try  {
+             const data = await  axios.get("http://localhost:8080/products/quant")
+             setTotalProducts(data.data)
+      } 
+             
+             catch(err){
+               console.error(err)
+             }
+         }
+     
+         useEffect(()=>{
+          GetTotalProductsQuantity();
+         },[totalProducts] )
+
+         const [totalSuppliers,setTotalSuppliers] = useState(0);
+         async function  GetTotalSuppliersQuantity(){
+           try  {
+                  const data = await  axios.get("http://localhost:8080/suplliers/quant")
+                  setTotalSuppliers(data.data)
+           } 
+                  
+                  catch(err){
+                    console.error(err)
+                  }
+              }
+          
+              useEffect(()=>{
+                GetTotalSuppliersQuantity();
+              },[totalSuppliers] )
+
+
   
   return (
     <div className='section'>
           <div className='smallContainer' > 
-        <div className='smallBox'> <p>Total of suppliers</p> <span>3</span>  </div>
-        <div className='smallBox'>  <p>Total of products</p> <span>4</span>     </div>
+        <div className='smallBox'> <p>Total of suppliers</p> <span>{totalSuppliers}</span>  </div>
+        <div className='smallBox'>  <p>Total of products</p> <span>{totalProducts}</span>     </div>
 </div>
 
 
-        <div className='largeBox' >   <p>Total stock value</p> <span>$ {totalValue}</span>       </div>
+        <div className='largeBox' >   <p>Total stock value</p> <span>$ {totalValue ? totalValue : '0'}</span>       </div>
     
       </div>
   );
